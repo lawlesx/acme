c70f1dbb4723964eef618f488a628258aee0ea0e
Aniruddha Sil <welcometohellv@gmail.com>
2024-07-25 01:26:50 +0530

    style(TrustedBy.tsx): :art: added grid pattern

--- src/components/TrustedBy.tsx
+++ src/components/TrustedBy.tsx
@@ -1,8 +1,32 @@
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
+import GridPattern from './magicui/grid-pattern'
 
 const TrustedBy = () => {
   return (
-    <section className='flex h-[70vh] w-full flex-col items-center justify-center gap-10 overflow-hidden lg:flex-row lg:gap-20'>
+    <section className='relative flex h-[70vh] w-full flex-col items-center justify-center gap-10 overflow-hidden lg:flex-row lg:gap-20'>
+      <GridPattern
+        width={30}
+        height={30}
+        x={-1}
+        y={-1}
+        strokeDasharray={'4 2'}
+        squares={[
+          [4, 4],
+          [5, 1],
+          [8, 2],
+          [5, 3],
+          [5, 5],
+          [10, 10],
+          [12, 15],
+          [15, 10],
+          [10, 15],
+          [15, 10],
+          [10, 15],
+          [15, 10],
+        ]}
+        className={cn('[mask-image:linear-gradient(to_bottom,transparent,white,transparent)]')}
+      />
       <h1 className='text-center text-3xl font-semibold text-white md:text-left'>Trusted by thousands of teams.</h1>
       <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
         {[
