f9f5246c7d5f7baaefe9444f635aaff0ca44bbde
Aniruddha Sil <welcometohellv@gmail.com>
2024-07-26 00:25:01 +0530

    refactor(AnimatedGrid.tsx): :pencil2: change notes demo name

--- src/components/Features/AnimatedGrid.tsx
+++ src/components/Features/AnimatedGrid.tsx
@@ -8,23 +8,23 @@ import NotificationList from '../ProjectUpdates/NotificationList'
 
 const files = [
   {
-    name: 'bitcoin.pdf',
+    name: 'Bitcoin',
     body: 'Bitcoin is a cryptocurrency invented in 2008 by an unknown person or group of people using the name Satoshi Nakamoto.',
   },
   {
-    name: 'finances.xlsx',
+    name: 'Finances',
     body: 'A spreadsheet or worksheet is a file made of rows and columns that help sort data, arrange data easily, and calculate numerical data.',
   },
   {
-    name: 'logo.svg',
+    name: 'SVG',
     body: 'Scalable Vector Graphics is an Extensible Markup Language-based vector image format for two-dimensional graphics with support for interactivity and animation.',
   },
   {
-    name: 'Sprint Planning.docx',
+    name: 'Sprint Planning',
     body: 'Tasks are broken down into small, manageable parts, and the team plans which items to complete during the next sprint.',
   },
   {
-    name: 'seed.txt',
+    name: 'Seed',
     body: 'A seed phrase, seed recovery phrase or backup seed phrase is a list of words which store all the information needed to recover Bitcoin funds on-chain.',
   },
 ]
