e44b60534caab8a8610c419ca8b9350f6ad6b723
Aniruddha Sil <welcometohellv@gmail.com>
2024-07-26 11:50:05 +0530

    style: :art: added subtle animation for first look

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,22 +1,42 @@
+'use client'
+import { motion } from 'framer-motion'
 import { ChevronRight } from 'lucide-react'
 import Image from 'next/image'
+import { useEffect, useState } from 'react'
 import AnimatedGradientText from './magicui/animated-gradient-text'
 import Particles from './magicui/particles'
 import SparklesText from './magicui/sparkles-text'
 import WordPullUp from './magicui/word-pull-up'
 
 const Hero = () => {
+  const [showWordsPullUp, setShowWordsPullUp] = useState(false)
+
+  useEffect(() => {
+    // Show the WordPullUp component after 1s
+    const timeout = setTimeout(() => {
+      setShowWordsPullUp(true)
+    }, 1000)
+
+    return () => clearTimeout(timeout)
+  }, [])
+
   return (
     <section className='relative z-0 min-h-screen w-full overflow-hidden'>
       <div className='flex h-[80vh] flex-col items-center justify-start gap-10 pt-20'>
-        <div className='absolute top-[30%] z-[1] h-full w-[200vw] [mask-image:linear-gradient(to_top,transparent_30%,black_100%)] sm:top-[40%] md:w-full'>
+        <motion.div
+          initial={{ opacity: 0, y: 100 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.5, duration: 1 }}
+          className='absolute top-[30%] z-[1] h-full w-[200vw] [mask-image:linear-gradient(to_top,transparent_30%,black_100%)] sm:top-[40%] md:w-full'
+        >
           <Image
             src='https://linear.app/cdn-cgi/imagedelivery/fO02fVwohEs9s9UHFwon6A/d4c9c051-1515-488f-00aa-1553b9322900/f=auto,dpr=2,q=95,fit=scale-down,metadata=none'
             alt='ui'
             fill
             className='object-contain lg:object-cover'
+            priority
           />
-        </div>
+        </motion.div>
         <Particles
           className='absolute inset-0'
           quantity={50}
@@ -28,39 +48,44 @@ const Hero = () => {
           size={1.5}
         />
         <Particles className='absolute inset-0' quantity={100} ease={80} color='#FFAE03' refresh vx={-0.2} vy={-0.2} />
-        <h1 className='relative z-[2] flex flex-col gap-4 text-center text-5xl font-thin text-white'>
+        <motion.h1
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          className='relative z-[2] flex flex-col gap-4 text-center text-5xl font-thin text-white'
+        >
           Capture Every{' '}
           <span className='flex flex-col gap-4 font-bold lg:flex-row'>
             {['Thought', 'Anywhere', 'Anytime.'].map((item, i) => (
               <SparklesText key={i} text={item} sparklesCount={4} colors={{ first: '#FF0F80', second: '#FFAE03' }} />
             ))}
           </span>
-        </h1>
-        <WordPullUp
-          className='z-[2] w-full px-4 text-center text-xl font-medium tracking-[0.04em] text-white md:text-3xl md:leading-[2rem]'
-          words='Change the way you take NOTES.'
-        />
-        {/* <div className='z-[1] mx-auto flex w-full flex-col items-center justify-center gap-6 md:flex-row md:gap-10'>
+        </motion.h1>
+        {showWordsPullUp ? (
           <WordPullUp
-            className='w-4/5 px-4 text-center text-xl font-medium tracking-[0.04em] text-white md:w-[25rem] md:text-3xl md:leading-[2rem]'
+            className='z-[2] w-full px-4 text-center text-xl font-medium tracking-[0.04em] text-white md:text-3xl md:leading-[2rem]'
             words='Change the way you take NOTES.'
           />
-          <div className='hidden h-full w-[2px] bg-white/80 md:block' />
-          <p className='w-4/5 text-center text-base text-[#D9D9D9] md:w-[25rem]'>
-            Capture and organize ideas effortlessly with AI-powered assistance and secure protection. Fast, ergonomic,
-            and reliable.
-          </p>
-        </div> */}
-        <AnimatedGradientText className='z-[2] cursor-pointer rounded-full bg-transparent py-2 backdrop-blur-sm'>
-          🎉{' '}
-          <h2 className='animate-gradient bg-gradient-to-r from-chili_red via-rose to-aerospace_orange bg-[length:var(--bg-size)_100%] bg-clip-text pl-1 font-bold text-transparent'>
-            Request a demo
-          </h2>
-          <ChevronRight
-            className='ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5'
-            color='#FF0F80'
-          />
-        </AnimatedGradientText>
+        ) : (
+          <div className='h-8' />
+        )}
+        <motion.div
+          className='z-[2]'
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5, duration: 0.5 }}
+        >
+          <AnimatedGradientText className='cursor-pointer rounded-full bg-transparent py-2 backdrop-blur-sm'>
+            🎉{' '}
+            <h2 className='animate-gradient bg-gradient-to-r from-chili_red via-rose to-aerospace_orange bg-[length:var(--bg-size)_100%] bg-clip-text pl-1 font-bold text-transparent'>
+              Request a demo
+            </h2>
+            <ChevronRight
+              className='ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5'
+              color='#FF0F80'
+            />
+          </AnimatedGradientText>
+        </motion.div>
       </div>
     </section>
   )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { motion } from 'framer-motion'
 import Link from 'next/link'
 import Logo from './Logo'
 import ShimmerButton from './magicui/shimmer-button'
@@ -5,28 +7,35 @@ import ShineBorder from './magicui/shine-border'
 
 const Navbar = () => {
   return (
-    <ShineBorder
-      className='sticky top-5 z-[1] flex w-full items-center overflow-hidden rounded-lg border border-rose-100 bg-transparent px-10 backdrop-blur-xl md:w-1/2 lg:w-3/5'
-      color={['#E9190F', '#FF0F80', '#FE4E00', '#E67F0D', '#FFAE03']}
+    <motion.div
+      className='sticky top-4 z-[1] flex w-full items-center justify-center'
+      initial={{ y: -100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ delay: 1.5, duration: 0.5 }}
     >
-      <nav className='z-[1] flex w-full items-center justify-between'>
-        <div className='flex items-center gap-2'>
-          <Logo className='h-12 w-12' />
-          <h1 className='pt-1 text-2xl font-medium text-white'>Acme</h1>
-        </div>
-        <div className='hidden gap-4 md:flex'>
-          {navItems.map((item, i) => (
-            <Link key={i} href='#' className='group tracking-widest text-white transition-colors'>
-              {item}
-              <div className='h-[2px] w-0 rounded-full bg-aerospace_yellow transition-all group-hover:w-3/4' />
-            </Link>
-          ))}
-        </div>
-        <ShimmerButton shimmerColor='#FF0F80' shimmerSize='0.1em' className='py-2'>
-          Get Started
-        </ShimmerButton>
-      </nav>
-    </ShineBorder>
+      <ShineBorder
+        className='flex w-full items-center overflow-hidden rounded-lg border border-rose-100 bg-transparent px-10 backdrop-blur-xl md:w-1/2 lg:w-3/5'
+        color={['#E9190F', '#FF0F80', '#FE4E00', '#E67F0D', '#FFAE03']}
+      >
+        <nav className='z-[1] flex w-full items-center justify-between'>
+          <div className='flex items-center gap-2'>
+            <Logo className='h-12 w-12' />
+            <h1 className='pt-1 text-2xl font-medium text-white'>Acme</h1>
+          </div>
+          <div className='hidden gap-4 md:flex'>
+            {navItems.map((item, i) => (
+              <Link key={i} href='#' className='group tracking-widest text-white transition-colors'>
+                {item}
+                <div className='h-[2px] w-0 rounded-full bg-aerospace_yellow transition-all group-hover:w-3/4' />
+              </Link>
+            ))}
+          </div>
+          <ShimmerButton shimmerColor='#FF0F80' shimmerSize='0.1em' className='py-2'>
+            Get Started
+          </ShimmerButton>
+        </nav>
+      </ShineBorder>
+    </motion.div>
   )
 }
 
